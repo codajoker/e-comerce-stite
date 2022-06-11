a77491d92740d89c0418146fa9642516eaff7a62
codajoker <codajoker91@gmail.com>
2022-06-11 15:34:48 +0300

    1

---
+++ src/components/Products/Product/Product.tsx
@@ -0,0 +1,79 @@
+import { KeyboardEvent } from 'react';
+
+import formatPrice from 'utils/formatPrice';
+import { IProduct } from 'models';
+
+import { useCart } from 'contexts/cart-context';
+
+import * as S from './style';
+
+interface IProps {
+  product: IProduct;
+}
+
+const Product = ({ product }: IProps) => {
+  const { openCart, addProduct } = useCart();
+  const {
+    sku,
+    title,
+    price,
+    installments,
+    currencyId,
+    currencyFormat,
+    isFreeShipping,
+  } = product;
+
+  const formattedPrice = formatPrice(price, currencyId);
+  let productInstallment;
+
+  if (installments) {
+    const installmentPrice = price / installments;
+
+    productInstallment = (
+      <S.Installment>
+        <span>or {installments} x</span>
+        <b>
+          {currencyFormat}
+          {formatPrice(installmentPrice, currencyId)}
+        </b>
+      </S.Installment>
+    );
+  }
+
+  const handleAddProduct = () => {
+    addProduct({ ...product, quantity: 1 });
+    openCart();
+  };
+
+  const handleAddProductWhenEnter = (event: KeyboardEvent) => {
+    if (event.key === 'Enter' || event.code === 'Space') {
+      addProduct({ ...product, quantity: 1 });
+      openCart();
+    }
+  };
+
+  return (
+    <S.Container onKeyUp={handleAddProductWhenEnter} sku={sku} tabIndex={1}>
+      {isFreeShipping && <S.Stopper>Free shipping</S.Stopper>}
+      <S.Image alt={title} />
+      <S.Title>{title}</S.Title>
+      <S.Price>
+        <S.Val>
+          {console.log(Number(formattedPrice) + 5)}
+          <small>{currencyFormat}</small>
+          <b>
+            {Number(formattedPrice.substring(0, formattedPrice.length - 3)) +
+              440}
+          </b>
+          <span>{formattedPrice.substring(formattedPrice.length - 3)}</span>
+        </S.Val>
+        {productInstallment}
+      </S.Price>
+      <S.BuyButton onClick={handleAddProduct} tabIndex={-1}>
+        Add to cart
+      </S.BuyButton>
+    </S.Container>
+  );
+};
+
+export default Product;
